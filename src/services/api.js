da8db433e2e55f99ff97cac9ab2647176d14c9fb
prakashvenkatesan-svg <prakash.venkatesan@aionioncapital.com>
2026-07-03 22:38:45 +0530

    Fix production API URL routing after colleague's merge

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 
-export const getApiBaseUrl = () =>
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
+const getApiBaseUrl = () => {
+  if (process.env.REACT_APP_API_BASE_URL) {
+    return process.env.REACT_APP_API_BASE_URL;
+  }
 
+  if (typeof window !== "undefined") {
+    const { hostname } = window.location;
+    if (["localhost", "127.0.0.1"].includes(hostname)) {
+      return "http://localhost:5000/api";
+    }
+  }
+
+  return "https://57yp657i65.execute-api.ap-south-1.amazonaws.com/staging/api";
+};
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: getApiBaseUrl(),
 });
 
 api.interceptors.request.use((config) => {
@@ -20,3 +31,5 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
+export { getApiBaseUrl };
