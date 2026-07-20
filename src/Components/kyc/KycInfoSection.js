60dd676bc0e4e0cbd7d03132652ed30211889eec
prakashvenkatesan-svg <prakash.venkatesan@aionioncapital.com>
2026-07-20 12:32:30 +0530

    fix: update contact email to aionioncapital.com

--- src/Components/kyc/KycInfoSection.js
+++ src/Components/kyc/KycInfoSection.js
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import exclamatory from "../../assets/exclamatory.png";
 
 const KycInfoSection = () => {
-  
+
 
   return (
-   
-     
-      <div className='registration-right'>
-        <p className='feedback-text'>
-          For any feedback on the account opening process or other inquiries,
-          please feel free to reach out to us at <br />
-          <a href='mailto:feedback@anioncapital.com' className='mail'>
-            feedback@anioncapital.com
-          </a>
-        </p>
-      </div>
-    
+
+
+    <div className='registration-right'>
+      <p className='feedback-text'>
+        For any feedback on the account opening process or other inquiries,
+        please feel free to reach out to us at <br />
+        <a href='mailto:feedback@aionioncapital.com' className='mail'>
+          feedback@aionioncapital.com
+        </a>
+      </p>
+    </div>
+
   );
 };
 
